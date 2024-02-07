960282ef090de4cc819a257742dfb128ff542474
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-02-07 12:18:43 +0000

    arbitrary data may be attested to

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const c = require('compact-encoding')
 const b4a = require('b4a')
 
 const KeyChain = require('./lib/keychain')
-const { sign, verify } = require('./lib/crypto')
-
-const { ProofEncoding } = require('./lib/encoding')
+const { sign, verify, hash } = require('./lib/crypto')
 
 const {
+  ProofEncoding,
+  AttestedData,
+  AttestedDevice
+} = require('./lib/encoding')
+
+const {
+  ATTESTED_DEVICE,
+  ATTESTED_DATA,
   VERSION,
   KEET_ROOT_PATH,
   KEET_DISCOVERY_PATH,
@@ -57,9 +63,11 @@ module.exports = class IdentityKey {
       proof = c.decode(ProofEncoding, proof)
     }
 
-    const signature = sign(publicKey, parent)
+    const signable = c.encode(AttestedDevice, publicKey)
+    const signature = sign(signable, parent)
 
     proof.chain.push({
+      type: ATTESTED_DEVICE,
       publicKey,
       signature
     })
@@ -67,40 +75,86 @@ module.exports = class IdentityKey {
     return c.encode(ProofEncoding, proof)
   }
 
-  static verify (proof, opts = {}) {
+  static attestData (attestedData, keyPair, proof) {
+    if (attestedData === null) throw new Error('Data is not attestable')
+
     if (b4a.isBuffer(proof)) {
       proof = c.decode(ProofEncoding, proof)
     }
 
-    if (proof.version !== VERSION) return null
+    const signable = c.encode(AttestedData, hash(attestedData))
+    const signature = sign(signable, keyPair)
 
+    proof.chain.push({
+      type: ATTESTED_DATA,
+      signature
+    })
+
+    return c.encode(ProofEncoding, proof)
+  }
+
+  static verify (proof, attestedData, opts = {}) {
+    if (b4a.isBuffer(proof)) {
+      proof = c.decode(ProofEncoding, proof)
+    }
+
+    const { version, timestamp, root, chain } = proof
+
+    // validate version
+    if (version > VERSION) return null
+
+    // validate attested data
+    if (!validateAdditionalData(chain, attestedData)) return null
+
+    // verify timestamp
     if (opts.timestamp) {
-      if (proof.timestamp < opts.timestamp) return null
+      if (timestamp < opts.timestamp) return null
     }
 
+    // verify root
     if (opts.identityPublicKey) {
-      if (!b4a.equals(proof.root, opts.identityPublicKey)) return null
+      if (!b4a.equals(root, opts.identityPublicKey)) return null
     }
 
-    const last = proof.chain[proof.chain.length - 1]
+    const devicePublicKey = attestedData === null
+      ? chain[chain.length - 1].publicKey
+      : chain[chain.length - 2].publicKey
 
     if (opts.devicePublicKey) {
-      if (!b4a.equals(last.publicKey, opts.devicePublicKey)) return null
+      if (!b4a.equals(devicePublicKey, opts.devicePublicKey)) return null
     }
 
-    for (let i = proof.chain.length - 1; i >= 0; i--) {
-      const { publicKey, signature } = proof.chain[i]
-      const parent = i === 0 ? proof.root : proof.chain[i - 1].publicKey
+    let parent = root
 
-      if (!verify(publicKey, signature, parent)) {
+    // verify chain
+    for (let i = 0; i < chain.length; i++) {
+      const { publicKey, signature } = chain[i]
+
+      const signable = publicKey
+        ? c.encode(AttestedDevice, publicKey)
+        : c.encode(AttestedData, hash(attestedData))
+
+      if (!verify(signable, signature, parent)) {
         return null
       }
+
+      parent = publicKey
     }
 
     return {
-      timestamp: proof.timestamp,
-      identityPublicKey: proof.root,
-      devicePublicKey: last.publicKey
+      timestamp,
+      identityPublicKey: root,
+      devicePublicKey
     }
   }
+}
+
+function validateAdditionalData (chain, attestedData) {
+  const last = chain[chain.length - 1]
+
+  if (!attestedData && attestedData !== null) return false
+  if (attestedData === null && last.publicKey === null) return false
+  if (attestedData !== null && last.publicKey !== null) return false
+
+  return true
 }
--- test/basic.js
+++ test/basic.js
@@ -1,5 +1,6 @@
 const test = require('brittle')
 const crypto = require('hypercore-crypto')
+const b4a = require('b4a')
 
 const IdentityKey = require('../')
 const KeyChain = require('../lib/keychain')
@@ -12,7 +13,7 @@ test('basic', function (t) {
   const { identityPublicKey } = IdentityKey.from({ mnemonic })
 
   const proof = IdentityKey.bootstrap({ mnemonic }, publicKey)
-  const auth = IdentityKey.verify(proof)
+  const auth = IdentityKey.verify(proof, null)
 
   t.unlike(auth, null)
   t.alike(auth && auth.devicePublicKey, publicKey)
@@ -25,7 +26,7 @@ test('basic - timestamp fail', function (t) {
   const { publicKey } = crypto.keyPair()
 
   const proof = IdentityKey.bootstrap({ mnemonic }, publicKey)
-  const auth = IdentityKey.verify(proof, { timestamp: Date.now() + 1 })
+  const auth = IdentityKey.verify(proof, null, { timestamp: Date.now() + 1 })
 
   t.alike(auth, null)
 })
@@ -36,7 +37,7 @@ test('basic - root fail', function (t) {
   const { publicKey } = crypto.keyPair()
 
   const proof = IdentityKey.bootstrap({ mnemonic }, publicKey)
-  const auth = IdentityKey.verify(proof, { identityPublicKey: publicKey })
+  const auth = IdentityKey.verify(proof, null, { identityPublicKey: publicKey })
 
   t.alike(auth, null)
 })
@@ -52,9 +53,41 @@ test('basic - device authenticates another device', function (t) {
   const proof1 = IdentityKey.bootstrap({ mnemonic }, device1.publicKey)
   const proof2 = IdentityKey.attest(device2.publicKey, device1, proof1)
 
-  const auth = IdentityKey.verify(proof2)
+  const auth = IdentityKey.verify(proof2, null)
 
   t.unlike(auth, null)
   t.alike(auth && auth.devicePublicKey, device2.publicKey)
   t.alike(auth && auth.identityPublicKey, identityPublicKey)
+})
+
+test('basic - device attests data', function (t) {
+  const mnemonic = IdentityKey.generateMnemonic()
+
+  const device1 = KeyChain.from({ seed: crypto.randomBytes(32) })
+  const attestData = b4a.from('attested data')
+
+  const { identityPublicKey } = IdentityKey.from({ mnemonic })
+
+  const proof1 = IdentityKey.bootstrap({ mnemonic }, device1.publicKey)
+  const proof2 = IdentityKey.attestData(attestData, device1, proof1)
+
+  const auth = IdentityKey.verify(proof2, attestData)
+
+  t.unlike(auth, null)
+  t.alike(auth && auth.devicePublicKey, device1.publicKey)
+  t.alike(auth && auth.identityPublicKey, identityPublicKey)
+})
+
+test('basic - attested data fail', function (t) {
+  const mnemonic = IdentityKey.generateMnemonic()
+
+  const device1 = KeyChain.from({ seed: crypto.randomBytes(32) })
+  const attestData = b4a.from('attested data')
+
+  const proof1 = IdentityKey.bootstrap({ mnemonic }, device1.publicKey)
+  const proof2 = IdentityKey.attestData(attestData, device1, proof1)
+
+  const auth = IdentityKey.verify(proof2, b4a.from('not attested data'))
+
+  t.alike(auth, null)
 })
